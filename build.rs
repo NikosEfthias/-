9b34a2c1e56cef7f50293fdf579a2a06c208015c
nikos efthias <nikos@mugsoft.io>
2020-04-27 18:54:14 +0400

    initial view

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
+use std::process::Command;
 fn main() {
-    println!("cargo:rustc-link-search=./scitter/");
+    Command::new("cp")
+        .args(&[
+            "./sciter/libsciter-gtk.so",
+            "./target/x86_64-unknown-linux-gnu/debug/",
+        ])
+        .spawn()
+        .unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-// vi: wildignore+=*/scitter/doc/*
-#[link(name = "sciter")]
-extern "C" {}
+use sciter::window;
+const UI: &[u8] = include_bytes!("../ui/index.html");
 fn main() {
-    println!("Hello, world!");
+    let mut win = window::Builder::main_window()
+        .closeable()
+        .glassy()
+        .resizeable()
+        .with_title()
+        .debug()
+        .create();
+    win.set_options(window::Options::DebugMode(true)).unwrap();
+    win.set_title("ΤΑΧΥΔΡΟΜΟΣ");
+    win.load_html(UI, None);
+    win.run_app();
 }
+// vi: wildignore+=*/sciter/*
